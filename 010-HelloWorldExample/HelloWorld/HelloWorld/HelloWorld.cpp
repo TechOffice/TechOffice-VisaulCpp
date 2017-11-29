a90acb64088e89a48f5d54d84e6ae5a69fc904fb
imben1109 <imben1109@gmail.com>
2017-11-30 02:40:19 +0800

    update

---
+++ 010-HelloWorldExample/HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,16 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	int i;
+	std::cout << "Hello World" << std::endl;
+	std::cout << "Please enter any character and press enter to exit this program!" << std::endl;
+	std::cin >> i;
+    return 0;
+}
+
