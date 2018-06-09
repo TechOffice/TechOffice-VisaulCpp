9e99c3a9ee6649a9fdee300728f54e5ff9bad0c0
ben <ben@techoffice.com>
2018-06-09 22:03:53 +0800

    update

---
+++ 020-WinApi/010-Networking/010-Wifi/WlanAvailableNetworkExample/WlanAvailableNetworkExample/WlanExample.cpp
@@ -0,0 +1,39 @@
+// WlanExample.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <windows.h>
+#include <wlanapi.h>
+#include <string>
+#pragma comment(lib, "wlanapi.lib")
+
+int main()
+{
+	PWLAN_INTERFACE_INFO_LIST interfaceList = NULL;
+	DWORD clientVersion = 2;
+	DWORD wlanApiVersion = 0;
+	HANDLE clientHandle = NULL;
+	WlanOpenHandle(clientVersion, NULL, &wlanApiVersion, &clientHandle);
+	WlanEnumInterfaces(clientHandle, NULL, &interfaceList);
+
+	std::cout << " Number of Interface: " << interfaceList->dwNumberOfItems << std::endl ;
+
+	for (int i = 0; i < (int)interfaceList->dwNumberOfItems; i++) {
+		PWLAN_INTERFACE_INFO interfaceInfo = (WLAN_INTERFACE_INFO *)&interfaceList->InterfaceInfo[i];
+		
+		// Get List
+		PWLAN_AVAILABLE_NETWORK_LIST availaleNetworkList = NULL;
+		WlanGetAvailableNetworkList(clientHandle, &interfaceInfo->InterfaceGuid, 0, NULL, &availaleNetworkList);
+		std::cout << " Number of Available Network: " << availaleNetworkList->dwNumberOfItems << std::endl;
+
+		for (int j = 0; j < (int)availaleNetworkList->dwNumberOfItems; j++) {
+			PWLAN_AVAILABLE_NETWORK networkEntry = (WLAN_AVAILABLE_NETWORK *)& availaleNetworkList->Network[j];
+			std::cout << networkEntry->dot11Ssid.ucSSID << std::endl;
+		}
+	}
+
+	std::cin.get();
+	return 0;
+}
+
